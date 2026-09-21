c0857dece6900f7cd98a9a6cc49c307bb9ba2ff2
Kevin Surya Prayoga Wibowo <kevin_surya_2004@student.uns.ac.id>
2026-09-21 15:45:10 +0700

    Add JADICEK_SPACE_ID for API configuration

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 // Kosong = gunakan /api/predict pada host website yang sama.
 // Untuk Hugging Face Space, isi contoh: "https://username-jadicek-api.hf.space"
+window.JADICEK_SPACE_ID = "kevin-surya04/jadicek-api";
+
 window.JADICEK_API_URL = "";
